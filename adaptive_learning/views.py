1ed40130cb8b8d0c32b9580ec2c366e0db645639
frankstanley60 <frankstanley60@gmail.com>
2024-06-18 13:42:18 +0300

    commit adaptive_learning

--- adaptive_learning/views.py
+++ adaptive_learning/views.py
@@ -2,12 +2,18 @@
 
 from django.shortcuts import render
 from .models import LearningItem, UserProgress
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
 
 def recommend_items(request):
     user_progress = UserProgress.objects.get(user=request.user)
     proficiency_level = user_progress.proficiency_level
     recommended_items = LearningItem.objects.filter(level__lte=proficiency_level)[:5]
-    return render(request, 'recommendations.html', {'recommended_items': recommended_items})
+    return redirect(reverse('content_management:lesson_list'))
+    #return render(request, 'recommendations.html', {'recommended_items': recommended_items})
 
 # views.py (continued)
 
@@ -15,5 +21,6 @@ def adaptive_learning(request):
     user_progress = UserProgress.objects.get(user=request.user)
     proficiency_level = user_progress.proficiency_level
     next_item = LearningItem.objects.filter(level__lte=proficiency_level).first()
-    return render(request, 'adaptive_learning.html', {'next_item': next_item})
+    return redirect(reverse('content_list'))
+    #return render(request, 'adaptive_learning.html', {'next_item': next_item})
 
